151a842df5546e224137a6d4fd6e40372b32e876
sholt0r <sholtor@kobu.au>
2024-10-10 21:37:39 +0000

    Updated error logging.

--- hta.py
+++ hta.py
@@ -9,7 +9,7 @@ def send_http_request(API_HOST, API_PORT, TOKEN, S_FUNC, LOGGER_NAME=None):
         json = {"function":f"{S_FUNC}"}
         response = re.post(f"https://{API_HOST}:{API_PORT}/api/v1", headers=headers, json=json)
         if response.status_code not in (200, 204):
-            logger.info(f"Status code {response.status_code}")
+            logger.error(f"Status code {response.status_code}")
             return False
         if S_FUNC == "QueryServerState":
             logger.info("Query command successful")
@@ -18,5 +18,5 @@ def send_http_request(API_HOST, API_PORT, TOKEN, S_FUNC, LOGGER_NAME=None):
             logger.info("Shutdown command successful")
             return True
     except Exception as err:
-        logger.info(f"{err}")
+        logger.error(f"{err}")
         return False
